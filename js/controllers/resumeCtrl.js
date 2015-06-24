e9f4c97a48059aab608fd8d26831f543a6e8cd26
Alex A <alexdevprojects@gmail.com>
2015-06-23 22:20:27 -0700

    Translucent bg, style updates, proofreading

--- js/controllers/resumeCtrl.js
+++ js/controllers/resumeCtrl.js
@@ -2,5 +2,5 @@ app.controller('ResumeController', ['$scope', function($scope) {
 
   $scope.title = "Résumé";
   $scope.resume_url = "https://docs.google.com/document/d/1MBaN2_a9Ti594xRPxt7J8DQenGzHing76j_FiN00fBA/edit?usp=sharing";
-
-}]);
+  $scope.resume_emb = "https://docs.google.com/document/d/1MBaN2_a9Ti594xRPxt7J8DQenGzHing76j_FiN00fBA/pub?embedded=true";
+  }]);
